78db49dafb9a39f310d6ba29d0216f160d9f45c6
Harsh <hk755063@gmail.com>
2026-03-10 23:20:15 +0530

    embedding generation optimization

--- src/hybrid_search.py
+++ src/hybrid_search.py
@@ -7,38 +7,42 @@
 # 5️⃣ compute hybrid score -> α * normalized_bm25 + β * semantic_similarity : (α = 0.7, β = 0.3)
 # 6️⃣ return ranked results
 
+import heapq
 from src.search import search
 from src.vector_search import vector_search
-import heapq
+from src.vector_index import generate_embeddings
 
-def hybrid_search(query, k=10, alpha = 0.7, beta = 0.3):
-    n = 10*k
-    
+
+def hybrid_search(query, k=10, alpha=0.7, beta=0.3):
+    n = 10 * k
+
     bm25_result = search(query, n)
-    vector_result = vector_search(query, n)
-    
+
+    search_vector = generate_embeddings([query])
+    vector_result = vector_search(search_vector, n)
+
     bm25_dict = {doc: score for doc, score in bm25_result}
-    vector_dict = {doc["doc_id"]: 1/(1+doc["distance"]) for doc in vector_result }
-        
+    vector_dict = {doc["doc_id"]: 1 / (1 + doc["distance"]) for doc in vector_result}
+
     candidates = set(bm25_dict) | set(vector_dict)
-    
+
     max_bm25 = max(bm25_dict.values()) if bm25_dict else 1
-    
+
     results = []
-    
+
     for doc in candidates:
-        bm25_score = bm25_dict.get(doc, 0)/max_bm25
+        bm25_score = bm25_dict.get(doc, 0) / max_bm25
         vector_score = vector_dict.get(doc, 0)
-        
-        hybrid_score = alpha*bm25_score + beta*vector_score
-        
+
+        hybrid_score = alpha * bm25_score + beta * vector_score
+
         results.append((doc, hybrid_score))
-        
-    results = heapq.nlargest(k, results, key=lambda x:x[1])       
-    
+
+    results = heapq.nlargest(k, results, key=lambda x: x[1])
+
     return results
 
-if __name__ == "__main__":
-    results = hybrid_search("Market is going to crash on monday") 
-    print(results)
 
+if __name__ == "__main__":
+    results = hybrid_search("Market is going to crash on monday")
+    print(results)
--- src/vector_search.py
+++ src/vector_search.py
@@ -1,29 +1,25 @@
 import faiss
 import json
-from src.vector_index import generate_embeddings
 import numpy as np
 
 index = faiss.read_index("storage/vector_index/faiss.index")
 with open("storage/vector_index/doc_ids.json") as f:
     doc_ids = json.load(f)
 
-def vector_search(text, k=5):
-    search_vector = generate_embeddings([text])
+
+def vector_search(search_vector, k=5):
     _vector = np.array(search_vector).astype("float32")
     faiss.normalize_L2(_vector)
 
     distances, ann = index.search(_vector, k=k)
-    
+
     results = []
     for dist, idx in zip(distances[0], ann[0]):
-        results.append({
-            "doc_id": doc_ids[idx],
-            "distance": float(dist)
-        })
+        results.append({"doc_id": doc_ids[idx], "distance": float(dist)})
 
     return results
 
-    
+
 if __name__ == "__main__":
     result = vector_search("I am going becoming successful, awesome and highly skilled")
     print(result)
